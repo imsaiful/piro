f1d76674463f1ccf6cdbe63ab48e413c5fb63b0b
Syed Namdar Hussain <syednh110@gmail.com>
2018-08-09 13:59:16 +0530

    modify file

--- pironews/twitter_api/twitter_credentials.py
+++
@@ -1,7 +0,0 @@
-# YouTube Video: https://www.youtube.com/watch?v=rhBZqEWsZU4
-# Variables that contains the user credentials to access Twitter API 
-ACCESS_TOKEN = "804258028526522368-0bHL5szbFKM5PCF5RpJSWYYC3etU7uA"
-ACCESS_TOKEN_SECRET = "QHH8B9TlFx7cQalTdr5dYt3VYefqQKeP6mSmzdfG6qkpP"
-CONSUMER_KEY = "tD7EQnr0oHClPVZu85RVnTutG"
-CONSUMER_SECRET = "SlwfMY3GcQiuY9M2eVitbHqiUc3LgMpghciSkvIUD4Kzpa4lYO"
-
