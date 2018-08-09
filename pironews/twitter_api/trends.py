7aeb5ca8ab3ee78b17467d568a63099776bb08dd
Syed Namdar Hussain <syednh110@gmail.com>
2018-08-09 13:52:46 +0530

    modify file

--- pironews/twitter_api/trends.py
+++ pironews/twitter_api/trends.py
@@ -13,6 +13,79 @@ from time import sleep # used for rate limiting
 from time import gmtime, strftime # used for gathering time
 import twitter_credentials
 
+OAUTH2_TOKEN = 'https://api.twitter.com/oauth2/token'
+
+class Storage:
+    def __init__(self):
+        self.contents = ''
+        self.line = 0
+
+    def store(self, buf):
+        self.line = self.line + 1
+        self.contents = "%s%i: %s" % (self.contents, self.line, buf)
+
+    def __str__(self):
+        return self.contents
+
+
+def getYear():
+	return strftime("%Y", gmtime())
+
+def getMonth():
+	return strftime("%m", gmtime())
+
+def getDay():
+	return strftime("%d", gmtime())
+
+def getHour():
+	return strftime("%H", gmtime())
+
+def getMinute():
+	return strftime("%M", gmtime())	
+
+def generateFileName():
+	return getYear()+"-"+getMonth()+"-"+getDay()+""
+
+# grabs the rate limit remaining from the headers
+def grab_rate_limit_remaining(headers):
+	limit = ''
+	h = str(headers).split('\n')
+	for line in h:
+		if 'x-rate-limit-remaining:' in line:
+			limit = line[28:-1]
+	return limit
+
+# grabs the time the rate limit expires
+def grab_rate_limit_time(headers):
+	x_time = ''
+	h = str(headers).split('\n')
+	for line in h:
+		if 'x-rate-limit-reset:' in line:
+			x_time = line[24:-1]
+	return x_time
+
+# obtains the bearer token
+def get_bearer_token(consumer_key,consumer_secret):
+	# enconde consumer key
+	consumer_key = urllib.parse.quote(consumer_key)
+	# encode consumer secret
+	consumer_secret = urllib.parse.quote(consumer_secret)
+# 	print(type(consumer_secret))
+	# create bearer token
+	bearer_token = consumer_key+':'+consumer_secret
+	# base64 encode the token
+	base64_encoded_bearer_token = base64.b64encode(bearer_token.encode('utf-8'))
+    # set headers
+	headers = {
+		"Authorization": "Basic " + base64_encoded_bearer_token.decode('utf-8') + "",
+		"Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+		"Content-Length": "29"}
+
+	response = requests.post(OAUTH2_TOKEN, headers=headers, data={'grant_type': 'client_credentials'})
+	to_json = response.json()
+	return to_json['access_token']
+
+
 def grab_a_tweet(bearer_token, tweet_id):
 	# url
 	url = "https://api.twitter.com/1.1/trends/place.json"
